a8c40997587ebdd6f3887012e8dd1ba7eed33db7
DiegoArayaT <nachox341@gmail.com>
2025-03-18 22:33:02 +0000

    Complete Basic Node and Express module

--- myApp.js
+++ myApp.js
@@ -1,7 +1,12 @@
 require('dotenv').config()
+var bodyParser = require('body-parser')
+
 let express = require('express');
 let app = express();
 
+app.use(bodyParser.urlencoded({extended: false})) 
+
+app.use(bodyParser.json());
 
 app.use(function middleware(req, res, next){
     var string = req.method + " " + req.path + " - " + req.ip;
@@ -35,14 +40,30 @@ app.get('/now', function middleware(req, res, next) {
 })
 
 
+app.get("/:word/echo", (req, res) => {
+    const { word } = req.params;
+    res.json({echo : word});
+  });
 
 
 
+  app.get("/name", function(req, res) {
+    var firstName = req.query.first;
+    var lastName = req.query.last;
+    
+    res.json({
+        name : `${firstName} ${lastName}`
+    });
+  });
 
 
+app.post("/name", function(req, res) {
+    var firstName = req.body.first;
+    var lastName = req.body.last
+    var string = firstName + " " + lastName
 
-
-
+    res.json({name: string });
+})
 
 
 
